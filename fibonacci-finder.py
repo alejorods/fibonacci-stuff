6a5fcfe516ec27fbdf5230e5a0cc2be91584525f
Alejandro <jesusrods312@gmail.com>
2021-02-11 22:23:06 -0300

    Aumento +2 en el rango del for para poder eliminar el segundo loop

--- fibonacci-finder.py
+++ fibonacci-finder.py
@@ -1,32 +1,24 @@
 # Programa que le pide un número al usuario y le indica si el número ingresado 
 # forma parte de la secuencia de Fibonacci.
 
-flag = True
-while flag:
+while True:
     try:
         user_number = int(input("Ingrese un número: "))
-        flag = False
+        break
     except:
         print("Debe ingresar un número.")
 fibo_list = [0, 1]
-for i in range(2,6):
-    new_number = fibo_list[i-1] + fibo_list[i-2]
+for i in range(2,user_number+2):
+    new_number = fibo_list[i-1] + fibo_list[i-2]         
     if new_number <= user_number:
         fibo_list.append(new_number)
     else:
         break
-if user_number > 5:
-    for j in range(6,user_number):
-        new_number = fibo_list[j-1] + fibo_list[j-2]
-        if new_number <= user_number:
-            fibo_list.append(new_number)
-        else:
-            break
 if user_number in fibo_list:
-    index = fibo_list.index(user_number) + 1
+    position = fibo_list.index(user_number) + 1
     if user_number == 1:
-        print("El número", user_number, "es el elemento", index, "y", (index+1), "de la secuencia de Fibonacci.")
+        print("El número", user_number, "es el elemento", position, "y", (position+1), "de la secuencia de Fibonacci.")
     else:
-        print("El número", user_number, "es el elemento", index, "de la secuencia de Fibonacci.")
+        print("El número", user_number, "es el elemento", position, "de la secuencia de Fibonacci.")
 else:
     print("El número", user_number, "no está en la secuencia de Fibonacci.")
